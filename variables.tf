dbe2c2a3f18ef352b19106cc2822e55a49e1c426
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2019-03-14 17:19:16 -0700

    module: updating azure to use latest cloud APIs

--- variables.tf
+++ variables.tf
@@ -2,6 +2,16 @@ variable "cluster_name" {
   description = "Name of the DC/OS cluster"
 }
 
+variable "instance_nic_ids" {
+  description = "List of instance nic ids created by this module"
+  type        = "list"
+}
+
+variable "ip_configuration_names" {
+  description = "List of ip configuration names associated with the instance nic ids"
+  type        = "list"
+}
+
 variable "resource_group_name" {
   description = "Name of the azure resource group"
 }
@@ -14,4 +24,9 @@ variable "tags" {
   description = "Add custom tags to all resources"
   type        = "map"
   default     = {}
+}
+
+# Number of Instance
+variable "num" {
+  description = "How many instances should be created"
 }
